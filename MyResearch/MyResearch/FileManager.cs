bd215fbf8a50acfbf56f95e4f06fddb5f1e76f2f
Oleksii Shevchuk <alex2994@ukr.net>
2015-11-20 14:35:40 +0200

    second commit

--- MyResearch/MyResearch/FileManager.cs
+++ MyResearch/MyResearch/FileManager.cs
@@ -23,6 +23,16 @@ namespace MyResearch
             Console.WriteLine("Saved in binary format");
         }
 
-        
+        public static List<Employee> ReadFromFile(string path)
+        {
+            List<Employee> list = new List<Employee>();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (Stream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                list.Add((Employee)binaryFormatter.Deserialize(fileStream));
+            }
+            Console.WriteLine("Read wrom file is done");
+            return list;
+        }
     }
 }
